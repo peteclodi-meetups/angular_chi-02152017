b1af0c472cf5c51c40ef27a396844b3c322eb9c7
angular-cli <angular-cli@angular.io>
2017-02-02 23:38:00 -0600

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularChi02152017Page } from './app.po';
+
+describe('angular-chi-02152017 App', function() {
+  let page: AngularChi02152017Page;
+
+  beforeEach(() => {
+    page = new AngularChi02152017Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
